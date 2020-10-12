09154c0b410e90c89fb56051a743a7d8d1a4f764
Ganzaru <34183330+Ganzaru@users.noreply.github.com>
2020-10-12 19:56:51 +0200

    MAP@k added. code refactored to make it possible

--- week1/cbir_w1.py
+++ week1/cbir_w1.py
@@ -1,39 +1,38 @@
-import operator
 import pickle
 import os
-
-import cv2 as cv
-from .dbp import dbp
-# import numpy as np
+import dbp
+import ml_metrics as mlm
 
 if __name__ == '__main__':
 
-    print('*********************************************')
     print('*********************************************')
 
     bbdd_path = 'D:\MCV\M1\Project\BBDD'
 
     qsd1_path = 'D:\MCV\M1\Project\qsd1_w1'
+    image_set = sorted(os.listdir(qsd1_path))
 
     # load groundtruth images of the query dataset
     groundtruth_images = pickle.load(open(os.path.join(qsd1_path, "gt_corresps.pkl"), 'rb'))
 
-
     # for each query image, find the corresponding BBDD image
+    color_space = "RGB"
+    bbdd_histograms = dbp.process_db_as_histograms(bbdd_path, color_space)
 
-    bbdd_histograms = dbp.process_images_as_histograms(bbdd_path)
+    results = []
+    histograms = []
 
-    results = {}
-    filename = input("Enter file name of image:")
-    file_path = qsd1_path+''
-    for qsd1_filename in qsd1_filenames:
+    for qsd1_filename in image_set:
         if qsd1_filename.endswith('.jpg'):
             qsd1_number = int(qsd1_filename.replace('.jpg', ''))
-            qsd1_hist = dbp.compute_histogram(os.path.join(qsd1_path, qsd1_filename), color_space)
-            distances = {}
-            for bbdd in bbdd_histograms.keys():
-                distances[bbdd] = cv.compareHist(qsd1_hist, bbdd_histograms[bbdd], OPENCV_DISTANCE_METRICS[distance_metric])
+            histogram = dbp.compute_histogram(os.path.join(qsd1_path, qsd1_filename), color_space)
+            histograms.append([qsd1_number, histogram])
+            results.append([qsd1_number, dbp.find_similar(histogram, bbdd_histograms, "Hellinger")])
 
-            results[qsd1_number] = (sorted(distances.items(), key=operator.itemgetter(1), reverse=reverse))[:10]
 
-    print(results)
+    for t in results:
+        print(t)
+    print("MAP@k: ")
+    print(mlm.mapk(histograms, results, 200))
+
+
--- week1/dbp.py
+++ week1/dbp.py
@@ -1,5 +1,6 @@
 import os
 import operator
+
 import cv2 as cv
 
 OPENCV_COLOR_SPACES = {
@@ -17,60 +18,55 @@ OPENCV_DISTANCE_METRICS = {
     "Hellinger": cv.HISTCMP_BHATTACHARYYA
 }
 
-class dbp:
+def compute_histogram(image_path, color_space="RGB"):
+    """
+    compute_histogram()
 
-    def compute_histogram(self, image_path, color_space="RGB"):
-        """
-        compute_histogram()
+    Function to compute ...
+    """
 
-        Function to compute ...
-        """
+    hist_channels = [0]
+    hist_bins = [8]
+    hist_range = [0, 256]
 
-        hist_channels = [0]
-        hist_bins = [256]
-        hist_range = [0, 256]
+    # if not gray --> 3 channels (RGB, HSV, ...)
+    if color_space != "GRAY":
+        hist_channels = hist_channels + [1, 2]
+        hist_bins = hist_bins * 3  # hist_bins = [256, 256, 256]
+        hist_range = hist_range * 3  # hist_range = [0, 256, 0, 256, 0, 256]
 
-        # if not gray --> 3 channels (RGB, HSV, ...)
-        if color_space != "GRAY":
-            hist_channels = hist_channels + [1, 2]
-            hist_bins = hist_bins * 3  # hist_bins = [256, 256, 256]
-            hist_range = hist_range * 3  # hist_range = [0, 256, 0, 256, 0, 256]
+    img = cv.imread(image_path)
+    hist = cv.calcHist([cv.cvtColor(img, OPENCV_COLOR_SPACES[color_space])], hist_channels, None, hist_bins,
+                       hist_range)
+    hist = cv.normalize(hist, hist, alpha=0, beta=1,
+                        norm_type=cv.NORM_MINMAX).flatten()  # change histogram range from [0,256] to [0,1]
 
-        img = cv.imread(image_path)
-        hist = cv.calcHist([cv.cvtColor(img, OPENCV_COLOR_SPACES[color_space])], hist_channels, None, hist_bins,
-                           hist_range)
-        hist = cv.normalize(hist, hist, alpha=0, beta=1,
-                            norm_type=cv.NORM_MINMAX).flatten()  # change histogram range from [0,256] to [0,1]
+    return hist
 
-        return hist
+def process_db_as_histograms(path, color_space="RGB"):
+    image_set = sorted(os.listdir(path))
+    histograms = {}
 
-    def process_images_as_histograms(self, path, color_space="RGB"):
-        image_set = sorted(os.listdir(path))
-        histograms = {}
+    for filename in image_set:
+        if filename.endswith('.jpg'):
+            number = int(filename.replace('.jpg', '').replace('bbdd_', ''))
+            histogram = compute_histogram(os.path.join(path, filename), color_space)
+            histograms[number] = histogram
 
-        for filename in image_set:
-            if filename.endswith('.jpg'):
-                number = int(filename.replace('.jpg', '').replace('bbdd_', ''))
-                histogram = self.compute_histogram(os.path.join(path, filename), color_space)
-                histograms[number] = histogram
+    return histograms
 
-        return histograms
+def find_similar(hist, bbdd_histograms, distance_metric="Hellinger"):
 
-    def find_top_similar(self, image_name, folder_path, bbdd_histograms, k="10", distance_metric="Hellinger", color_space="RGB"):
-        results = {}
-        reverse = False
+    reverse = False
+    distances = []
 
-        if distance_metric in ("Correlation", "Intersection"):
-            reverse = True
+    if distance_metric in ("Correlation", "Intersection"):
+        reverse = True
 
-        if image_name.endswith('.jpg'):
-            number = int(image_name.replace('.jpg', ''))
-            hist = dbp.compute_histogram(os.path.join(folder_path, image_name), color_space)
+    for bbdd in bbdd_histograms.keys():
+        dist = cv.compareHist(hist, bbdd_histograms[bbdd], OPENCV_DISTANCE_METRICS[distance_metric])
+        distances.append([bbdd, dist])
 
-            distances = {}
-            for bbdd in bbdd_histograms.keys():
-                distances[bbdd] = cv.compareHist(hist, bbdd_histograms[bbdd], OPENCV_DISTANCE_METRICS[distance_metric])
+    distances.sort(key=operator.itemgetter(1), reverse=reverse)
 
-            results[number] = (sorted(distances.items(), key=operator.itemgetter(1), reverse=reverse))[:k]
-
-        return results
+    return distances
