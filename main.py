64fa9fac6cab21fa8d4316e578fcb81a7adb7856
Shaurya Chaudhuri <shaurya@workspan.com>
2020-10-21 17:21:27 +0200

    SC: Divide and concatenate histograms

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
 
 from week1 import run_w1 as w1
+from week2 import run_w2_1 as w2_1
 
 if __name__=="__main__":
 
     if "--week1" in sys.argv:
         w1.run()
+
+    if "--week2" in sys.argv:
+        w2_1.run()
