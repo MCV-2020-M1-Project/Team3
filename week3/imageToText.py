12f8bc9eb9a3e8eaec4ea921e40ff84ca2d22b88
DESKTOP_1\Ian <ian.riera.smolinska@gmail.com>
2020-10-28 17:49:31 +0100

    Update imageToText.py

--- week3/imageToText.py
+++ week3/imageToText.py
@@ -1,9 +1,28 @@
 import pytesseract as tess
-tess.pytesseract.tesseract_cmd = r'<path to teseract.exe>' #for windows users
+tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' #your path to tesseract for windows users
 from PIL import Image
 import operator
 import cv2 as cv
 import jellyfish as jel
+import os
+
+
+
+def get_bbdd_texts(bbdd_path):
+    bbdd_texts = {}
+    for image_filename in sorted(os.listdir(bbdd_path)):
+        if image_filename.endswith('.txt'):
+            image_id = int(image_filename.replace('.txt', '').replace('bbdd_', ''))
+            f = open(os.path.join(bbdd_path, image_filename), "r")
+            line= f.readline()
+            if line.strip():
+                bbdd_text = line.lower().replace("(","").replace("'","").replace(")","") #ignore case
+            else:
+                bbdd_text='empty'
+            bbdd_texts[image_id] = bbdd_text
+
+            
+    return bbdd_texts
 
 
 def get_text(img, text_box):
@@ -13,7 +32,7 @@ def get_text(img, text_box):
     br_y=text_box[1][1]
     
     roi=img[tl_x:br_x,tl_y:br_y]
-    text = tess.image_to_string(roi,lang='cat').rstrip() # removes the end whitespaces
+    text = tess.image_to_string(roi,lang='cat').rstrip().lower() # removes the end whitespaces and lower to ignore case
     
     return text
 
@@ -28,20 +47,26 @@ def get_text_distance(text_1,text_2,distance_metric="Levensthein"):
         distance=jel.damerau_levenshtein_distance(text_1,text_2)
         
     elif distance_metric=="Jaro":
-        distance=1/jel.jaro_distance(text_1,text_2)
+        distance=int(10/jel.jaro_distance(text_1,text_2))
     else:
         print('Metric doesn\'t exist')
     return distance
         
-def get_k_images(painting, text_box, bbdd_texts, k="10", distance_metric="Levensthein"):
+def get_k_images(painting, text_box, bbdd_texts, k=10, distance_metric="Levensthein"):
 
     text = get_text(painting, text_box)
     
     distances = {}
 
-    for bbdd_id, bbdd_texts in bbdd_texts.items():
-        distances[bbdd_id] = get_text_distance(text, bbdd_texts,distance_metric)
-
+    for bbdd_id, bbdd_text in bbdd_texts.items():
+        
+        if bbdd_text!='empty':
+            bbdd_text=bbdd_text.replace("(","").replace("'","").replace(")","")
+            distances[bbdd_id] = get_text_distance(text, bbdd_text.split(",")[1].rstrip(),distance_metric) #for week 3 dataset change to index 0 = author
+        
+        else:
+            distances[bbdd_id]=100
+            
     k_predicted_images = (sorted(distances.items(), key=operator.itemgetter(1), reverse=False))[:k]
 
     return [predicted_image[0] for predicted_image in k_predicted_images]
@@ -50,24 +75,11 @@ def get_k_images(painting, text_box, bbdd_texts, k="10", distance_metric="Levens
 
 ###----TEST AREA----
 img = cv.imread("data/qsd1_w2/00000.jpg") 
-roi = [0,105,110,574]
+roi = [[0,105],[110,574]]
+bbdd_path='data/BBDD/'
 
-text = get_text(img,roi)
+bbdd_texts=get_bbdd_texts(bbdd_path)
 
+predicted_paintings = get_k_images(img,roi,bbdd_texts)
+print(predicted_paintings)
 
-gt_text =  'Bodego IV'
-gt_text_case =  'bodego iv'
-fail_text =  'Bodega VV'
-
-
-dist_lev1=get_text_distance(gt_text,text)
-dist_lev2=get_text_distance(gt_text_case,text)
-dist_lev3=get_text_distance(fail_text,text)
-
-dist_ham1=get_text_distance(gt_text,text)
-dist_ham2=get_text_distance(gt_text_case,text)
-dist_ham3=get_text_distance(fail_text,text)
-
-
-print('{} has a levenshtein distances of {}, {} and {}. Also a hamming distances of {}, {} and {}'.format(text, dist_lev1,dist_lev2,dist_lev3,dist_ham1,dist_ham2,dist_ham3))
- 
