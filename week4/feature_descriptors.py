ed4081ca8132bab71b24bcb5206b6a0c097e28a5
Oriol Catalan <oriol.catalan@netcentric.biz>
2020-11-05 19:41:40 +0100

    Calculate SURF added gray scale processing for descriptors retrival

--- week4/feature_descriptors.py
+++ week4/feature_descriptors.py
@@ -2,10 +2,9 @@ import cv2 as cv
 
 
 def surf_descriptor(image, threshold=400):
-    cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    print('surf')
+    img_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     surf = cv.xfeatures2d.SURF_create(threshold, )
-    kp, des = surf.detectAndCompute(image, None)
+    kp, des = surf.detectAndCompute(img_gray, None)
     features=[]
 
     for k, d in (zip(kp,des)):
